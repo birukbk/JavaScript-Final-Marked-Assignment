2bcb4af3a1a4833c7b6cbace6c4c6971b0aec054
Biruk G <birukbk@gmail.com>
2016-07-25 16:46:52 +0100

    adds mandatory field for first name field

--- contactform.js
+++ contactform.js
@@ -1,18 +1,25 @@
 function init() {
     checkForm();
+    setHintForAllFields();
+    setFocus();
 }
 
 function checkForm() {
-    //textHint();
-    //textHint(document.getElementById("fristName"), "Enter your first name");
-    textHintForAllFields();
-
+	document.getElementById("contactForm").onsubmit=function(){
+  if(document.getElementById("firstName").value===""||document.getElementById("firstName").value==="Enter your first name"){
+  //if(getElementsValue("firstName")===""||getElementsValue("firstName")==="Enter your first name"){
+  showError("firstName");
+    return false;
+  } else {
+    return true;
+  }
+  //return false;
+  
+ };
 }
-
-
 function textHint(txtElem, defaultText) {
-    var defaultText;
-    var txtElem;
+    //var defaultText;
+    //var txtElem;
     txtElem.value = defaultText;
     txtElem.style.color = "#A8A8A8";
     txtElem.style.fontStyle = "italic";
@@ -31,15 +38,23 @@ function textHint(txtElem, defaultText) {
         }
     }
 }
-function textHintForAllFields(){
-	textHint(document.getElementById("fristName"), "Enter your first name");
+function setHintForAllFields(){
+	textHint(document.getElementById("firstName"), "Enter your first name");
 	textHint(document.getElementById("lastName"), "Enter your last name");
-	textHint(document.getElementById("healthAuthorityNumber"), "health Autorith Number");
-	textHint(document.getElementById("fristName"), "Enter your email");
+	textHint(document.getElementById("healthAuthorityNumber"), "e.g. ZHA346783");
 	textHint(document.getElementById("email"), "Enter your email");
 	textHint(document.getElementById("telephoneNumber"), "Enter your telephone number");
-
-
 }
-
+function setFocus(){
+	document.getElementById("firstName").focus();
+}
+function getElementsValue(element){
+	var element;
+	return document.getElementById(element).value;
+}
+function showError(errorId){
+	var er=errorId+"Error";
+	document.getElementById(er).style.display="inline";
+}
 window.onload = init;
+//window.onload=checkForm;
